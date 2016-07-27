235342d3dbbf52225f5c5a047d1da95876ed63bd
BZQ <jerryqueue@gmail.com>
2016-07-27 13:52:42 +0800

    correct index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Button from 'react-mdl-comps';
+import Button from './react-mdl-comps/Button';
 
 ReactDOM.render(<Button />, document.getElementById('anchor'));
